65d09adf7a3b340e1ba54773bf75a38554ac050e
Ans Rajput <66619639+ranaansshahzad@users.noreply.github.com>
2022-07-01 16:04:54 +0500

    Add files via upload

---
+++ Class.js
@@ -0,0 +1,32 @@
+// const data=require('./StudentJson.json');
+// console.log(data)
+
+// class Product {
+//     constructor(itemsname,prices,discount,itemscode){
+//         this.itemsname = itemsname;
+//         this.prices = prices;
+//         this.discount = discount;
+//         this.itemscode = itemscode;
+//     }
+//     get getDiscountValue(){
+//         return this.discount;
+//     }
+//     set setDiscountValue(value){
+//         this.discount = value;
+//     }
+// }
+
+// let pencil = new Product('pencil', 25 , 40, '488')
+
+class Hello{
+    message(){
+        console.log("Welcome back to our job");
+    }
+    sorry(){
+        console.log("hello Mr.Ans");
+    }
+}
+
+let wow = new Hello();
+wow.message();
+wow.sorry();
